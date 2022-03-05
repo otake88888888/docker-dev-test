1d4e32d9b21438f8f19a9a0b6e14135124f73374
0take <example@example>
2022-03-05 16:08:04 +0900

    fix nodejs server

--- nodejs/app-server/src/app.js
+++ nodejs/app-server/src/app.js
@@ -1,7 +1,7 @@
 const Redis = require("ioredis");
 const redis = new Redis("6379","noderedis");
 
-const main = async () => {
+const redisTest = async () => {
   await redis.set("key", "val");
   const result = await redis.get("key");
   console.log(result); //val
@@ -9,4 +9,20 @@ const main = async () => {
   redis.disconnect();
 };
 
-main();
+// redisTest();
+
+const http = require("http");
+const port = 10081;
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+
+  const responseMessage = "<h1>Hello World</h1>";
+  response.end(responseMessage);
+  console.log(`Sent a response : ${responseMessage}`);
+});
+
+server.listen(port, "0.0.0.0");
+console.log(`The server has started and is listening on port number: ${port}`);
